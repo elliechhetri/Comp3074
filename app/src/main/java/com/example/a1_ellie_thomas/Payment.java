cfee76b54efe2718902a7864c389099c2617afe4
Ellie Adhikari <elliechhetri4@gmail.com>
2025-10-09 12:57:52 -0400

    feat(model): add Payment class with basic pay calculation

--- app/src/main/java/com/example/a1_ellie_thomas/Payment.java
+++ app/src/main/java/com/example/a1_ellie_thomas/Payment.java
@@ -1,4 +1,29 @@
 package com.example.a1_ellie_thomas;
 
 public class Payment {
+    private String name;
+    private double hoursWorked;
+    private double hourlyRate;
+
+    public Payment(String name, double hoursWorked, double hourlyRate) {
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public double calculatePay() {
+        return hoursWorked * hourlyRate;
+    }
 }
