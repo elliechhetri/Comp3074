a5acf2c0f62d4d7af7a9f0c6aa96676c765e5c6f
Ellie Adhikari <elliechhetri4@gmail.com>
2025-10-09 19:01:42 -0400

    feat(ui): add DetailActivity and hook it up in manifest

---
+++ app/src/main/java/com/example/a1_ellie_thomas/DetailActivity.java
@@ -0,0 +1,12 @@
+package com.example.a1_ellie_thomas;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail); // uses res/layout/activity_detail.xml
+    }
+}
